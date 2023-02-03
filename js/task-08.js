5963dacc21f9112480fb367e34cd3b0d7b935b71
anastasia-demia <109302069+anastasia-demia@users.noreply.github.com>
2023-02-03 17:15:52 +0200

    update task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector(".login-form");
+
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    console.log(email.value, password.value);
+
+    if (email.value === "" || password.value === "") {
+        return alert("ALL FIELDS MUST BE FILLED IN <3")
+    }
+
+    const userInformation = { email: email.value, password: password.value }
+
+    console.log(userInformation);
+    event.currentTarget.reset();
+});
 
