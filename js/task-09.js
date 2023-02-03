1c72a4bb42f708788d7e0e29f7d8bfe74c7a9705
anastasia-demia <109302069+anastasia-demia@users.noreply.github.com>
2023-02-03 17:39:19 +0200

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -2,5 +2,17 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const countClick = button.addEventListener('click')
+const bodyEl = document.querySelector("body")
+const buttonEl = document.querySelector("button.change-color")
+const spanEl = document.querySelector("span.color")
 
+buttonEl.addEventListener("click", (event) => {
+  let color = getRandomHexColor();
+  spanEl.textContent = color;
+  bodyEl.style.backgroundColor = color;
+})
+
+buttonEl.addEventListener("click", (event) => {
+  let color = getRandomHexColor();
+  buttonEl.style.backgroundColor = color;
+})
