afd2b1b763082f62be17fe76bb7e4ae666d40058
anastasia-demia <109302069+anastasia-demia@users.noreply.github.com>
2023-02-26 14:39:08 +0200

    task 1 edited

--- js/task-01.js
+++ js/task-01.js
@@ -1,16 +1,7 @@
 const categoriesEl = document.querySelectorAll('li.item')
 console.log('Number of categories: ', categoriesEl.length)
 
-const categoryName = document.querySelectorAll('h2')
-
-console.log('Category: ', categoryName[0].textContent)
-const animalNumber = document.querySelectorAll('li.animal-item')
-console.log('Elements: ', animalNumber.length)
-
-console.log('Category: ', categoryName[1].textContent)
-const productNumber = document.querySelectorAll('li.product-item')
-console.log('Elements: ', productNumber.length)
-
-console.log('Category: ', categoryName[2].textContent)
-const technoNumber = document.querySelectorAll('li.techno-item')
-console.log('Elements: ', technoNumber.length)
+categoriesEl.forEach((element) => {
+    console.log("Category: ", element.querySelector("h2").textContent);
+    console.log("Elements: ", element.querySelectorAll("li").length);
+});
